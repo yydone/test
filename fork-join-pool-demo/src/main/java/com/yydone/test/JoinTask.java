4d617a2654a0985e52ca6b46c9bf6b356e469744
yydone <yunix@yydone.com>
2019-02-11 09:39:13 +0800

    init

---
+++ fork-join-pool-demo/src/main/java/com/yydone/test/JoinTask.java
@@ -0,0 +1,16 @@
+package com.yydone.test;
+
+public class AddThread implements Runnable {
+
+    private int x;
+    private int y;
+
+    public AddThread(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    @Override
+    public void run() {
+
+    }
+}
---
+++ fork-join-pool-demo/src/main/java/com/yydone/test/StreamTest.java
@@ -0,0 +1,4 @@
+package com.yydone.test;
+
+public class StreamTest {
+}
